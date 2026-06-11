22c6a68bafb9a9c1edc318ab4eb7760b3856fd8d
AMIR11ATE <amirtv820@gmail.com>
2026-06-11 11:42:09 +0330

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'AMIR11ATE AND YASIN16',
+      home: const HomePage(),
+    );
+  }
+}
