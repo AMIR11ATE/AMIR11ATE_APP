5ee6cc2eb1980c34660970f67f55678e97d60744
AMIR11ATE <amirtv820@gmail.com>
2026-06-11 11:46:36 +0330

    Create recovery.dart

---
+++ recovery.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RecoveryPage extends StatelessWidget {
+  const RecoveryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("ریکاوری")),
+    );
+  }
+}
